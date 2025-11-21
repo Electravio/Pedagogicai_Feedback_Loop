b695fd7c8568ec62f0da5f348122c63e924ef76c
Electravio <106938689+Electravio@users.noreply.github.com>
2025-11-21 19:55:10 +0800

    update with earlier code

--- db.py
+++ db.py
@@ -13,8 +13,7 @@ except ImportError:
     HAVE_BCRYPT = False
 
 
-DB_FILE = "users_chats.db"
-#DB_FILE = /home/adminuser/.streamlit/users_chats.db
+DB_FILE = os.path.join(os.path.expanduser("~"), ".streamlit", "users_chats.db")
 
 CSV_CHAT_LOG = "chat_feedback_log.csv"
 
@@ -734,3 +733,4 @@ else:
         conn.close()
     except Exception as e:
         print(f"DB init check skipped: {e}")
+
